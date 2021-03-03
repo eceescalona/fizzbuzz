d4e8211e284beb6202b8429876cb137263f1bebe
Ernesto Carmona <eceescalona@gmail.com>
2021-03-03 14:10:07 -0500

    FizzBuzz - Add a validator class. - Override the functions IsValid and FormatErrorMessage.

---
+++ FizzBuzzConsoleExercise/FizzBuzzExcercise.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzzConsoleExercise
+{
+    public interface FizzBuzzExcercise
+    {
+        []
+        public string Excercise(int[] array);
+    }
+}
---
+++ FizzBuzzConsoleExercise/Utils/ValidationData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.ComponentModel.DataAnnotations;
+
+namespace FizzBuzzConsoleExercise.Utils
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    internal class ValidationData : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            int[] array = value as int[];
+            if (array == null || array.Length <= 0)
+            {
+                return false;
+            }
+            else if (array.Length > 100)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return string.Format("The {0} parameter is not valid.", nameof(name));
+        }
+    }
+}
